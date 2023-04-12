5dd07fc1f6de17849f0ba93e647bb0ea62541dbf
Shystra <jlsp.2019@hotmail.com>
2023-04-12 19:42:08 -0300

    verifica se o disabled esta ativo

--- my-app/src/components/Button/Button.spec.jsx
+++ my-app/src/components/Button/Button.spec.jsx
@@ -32,5 +32,12 @@ describe('<Button />', () => {
         render (<Button text="Load more" disabled={true} />);
         const button = screen.getByRole('button', { name: /load more/i });
         expect(button).toBeDisabled();
+    });
+
+    it('should be enabled when disabled is false', () => {
+        render (<Button text="Load more" disabled={false} />);
+        const button = screen.getByRole('button', { name: /load more/i });
+        expect(button).toBeEnabled();
     })
+
 
