a9b7cdf62e1de195c6dd37362320160623a4a6ff
Shystra <jlsp.2019@hotmail.com>
2023-04-13 19:10:59 -0300

    testando imagem

--- my-app/src/components/PostCard/Post.spec.jsx
+++ my-app/src/components/PostCard/Post.spec.jsx
@@ -1,13 +1,13 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import {PostCard} from '.';
 import { postCardPropsMock } from './mock';
 
 const props = postCardPropsMock;
 
 describe('<PostCard />', () => {
-    it ('should render PostCard correctly', () => {
+    it('should render PostCard correctly', () => {
         render(<PostCard {...props} />);
         
-    
-    })
+        expect(screen.getByRole('img', { name: /TITLE 1/i })).toBeInTheDocument();
+    });
 });
