429a785cc157046ab1037041e6cd4ae6b4f0a4a7
Shystra <jlsp.2019@hotmail.com>
2023-04-13 21:04:16 -0300

    tipando as props

--- my-app/src/components/Button/index.jsx
+++ my-app/src/components/Button/index.jsx
@@ -1,3 +1,4 @@
+import P from 'prop-types';
 import './styles.css';
 
 //import { Component } from "react";
@@ -23,3 +24,13 @@ export const Button = ({ text, onClick, disabled }) => (
       {text}
     </button>
   );
+
+Button.defaltProps = {
+    disabled: false,
+};
+
+Button.propTypes = {
+    text: P.string.isRequired,
+    onClick: P.func.isRequired,
+    disabled: P.bool,
+};
